9ed9125a544e12dc22e83ea0da65018f4a1e826c
nathalielbn <nathalie.labonnote@ensta-paris.fr>
2022-04-19 16:35:00 +0200

    alleluia

--- Sudoku/diagnath.c
+++ Sudoku/diagnath.c
@@ -6,6 +6,36 @@
 #define MAX 9
 
 
+//FONCTION DANS MATRICE.C
+
+//crée la matrice de 0
+int** matrice_vierge (int t){
+	int** area = malloc(t*sizeof(int*));
+	for (int i = 0; i<t; i++){
+		area[i] = malloc(t*sizeof(int));
+	}
+
+	for (int i =0; i<t; i++){
+		for (int j = 0; j<t; j++){
+			area[i][j] = 0;
+		}
+	}
+	return(area);
+
+}
+
+
+
+//Fonction qui affiche la matrice
+void affichage(int t, int** matrice){
+	for (int i =0; i<t; i++){
+		for (int j =0; j<t; j++){
+			printf("%d ", matrice[i][j]);
+		}
+		printf("\n");
+	}
+
+}
 
 //fonction random
 int aleatoire() {
@@ -48,39 +78,68 @@ int* cas(int t, int* ligne){
 }
 
 
-void affichage(int* ligne, int t){
+void affichageligne(int* ligne, int t){
 	for (int i=0; i<t; i++){
 		printf("%d", ligne[i]);
 	}
 }
 
-int** carreau (int* ligne, int l){ //ligne = ligne de 9 chiffres aléa; l = 9 = taille
-	int** carr = malloc(l/3*sizeof(int*));
+int** carreau (int l){ //ligne = ligne de 9 chiffres aléa; l = 9 = taille
+	//on crée l'espace pour le carreau et on crée la ligne
+	int* ligne = malloc(l*sizeof(int));
+	ligne = cas(l, ligne);
+	int** carr = malloc((l/3)*sizeof(int*));
+	for (int i =0; i<l/3; i++){
+		carr[i] = malloc((l/3)*sizeof(int));
+	}
+
+	//On remplie le carreau à partir de la ligne
 	for (int i = 0; i<l/3; i++){
-		carr[1][i] = ligne[i];
+		carr[0][i] = ligne[i];
 	}
 	for (int i = 0; i<l/3; i++){
-		carr[2][i] = ligne[3+i];
+		carr[1][i] = ligne[3+i];
 	}
+	for (int i = 0; i<l/3; i++){
+		carr[2][i] = ligne[6+i];
+	}
+
+	return(carr);
 }
+
+
+//Foncion qui remplie la diag
+int ** diag (int** matrice, int t){
+	int** carr0 = carreau(t);
+	int** carr4 = carreau(t);
+	int** carr8 = carreau(t);
+	for(int i=0; i<3; i++){
+		for (int j =0; j<3; j++){
+			matrice[i][j] = carr0[i][j];
+			matrice[i+3][j+3] = carr4[i][j];
+			matrice[i+6][j+6] = carr8[i][j];
+		}
+	}
+	
+
+	return(matrice);
+}
+
+
+
 
 int main (){
 	srand(time(NULL));
-	int* ligne = malloc(taille*sizeof(int));
+	/*int* ligne = malloc(taille*sizeof(int));
 	for (int i =0; i<taille; i++){
 		ligne[i] =0;
 	}
 
-	printf("%d\n", compar(1, ligne, taille));
-
-	printf("ligne initiale");
-	affichage(ligne, taille);
-	printf("\n");
-
-
-	ligne = cas(taille, ligne);
-	printf("ligne finale");
-	affichage(ligne, taille);
-	printf("\n");
+	ligne = cas(taille, ligne);*/
+	/*int ** carr = carreau(taille);
+	affichage(taille/3, carr);*/
+	int** area = matrice_vierge(taille); 
+	area = diag(area, taille);
+	affichage(taille, area);
 
 }
