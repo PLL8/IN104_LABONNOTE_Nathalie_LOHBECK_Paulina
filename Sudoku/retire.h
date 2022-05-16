b90d7a883f750e30c87f038a9e583c609029c777
nathalielbn <nathalie.labonnote@ensta-paris.fr>
2022-05-16 10:41:11 +0200

    N

--- Sudoku/retire.h
+++ Sudoku/retire.h
@@ -3,6 +3,6 @@
 
 int* retire_ligne(int* ligne, int k, int t); //fonction qui retire un nombre k de cases dans une ligne
 
-int** retire (int** matrice, int k, int t);
+int** retire (int** matrice, int k, int t); //fonction qui retire k cases par ligne dans la matrice globale
 
 #endif
